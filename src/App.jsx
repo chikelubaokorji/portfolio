8112298db6384379b08c1ad24c89da2e586a5426
Chikeluba Okorji <46967296+chikelubaokorji@users.noreply.github.com>
2024-11-12 10:58:18 +0000

    Components first edit

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  name: "Chikeluba Okorji",
+  title: "Software Engineer - Data",
+  email: "chikeluba.okorji@gmail.com",
+  gitHub: "",
+  instagram: "",
+  linkedIn: "chikelubaokorji",
+  medium: "chikelubaokorji",
+  twitter: "chikelubaokorji",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
